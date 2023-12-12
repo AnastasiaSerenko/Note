dbd09fe6ed0137b1d6e4d41f78d9279913607961
AnastasiaSerenko <orangesun94@gmail.com>
2023-12-12 22:17:01 +0700

    fixed event onclick for id=addNote: creating tag 'div' with some content instead of tag 'img'

--- wwwroot/site.js
+++ wwwroot/site.js
@@ -7,9 +7,30 @@ document.getElementById("addNote").addEventListener("click", async () => {
         })
     });
     if (response.ok === true) {
-        const note = document.createElement("img")
+        const note = document.createElement("div");
         note.setAttribute("class", "note");
-        note.setAttribute("src", "img/note.png");
+
+        const note_text = document.createElement("p");
+        note_text.setAttribute("class", "note-text");
+        note_text.setAttribute("contenteditable", "true");
+        let text = document.createTextNode("Hello World");
+        note_text.appendChild(text);
+        note.appendChild(note_text);
+
+        const btn_edit = document.createElement("button");
+        btn_edit.setAttribute("class", "note-btn");
+        btn_edit.setAttribute("id", "btn-edit");
+        text = document.createTextNode("Edit");
+        btn_edit.appendChild(text);
+        note.appendChild(btn_edit);
+
+        const btn_delete = document.createElement("button");
+        btn_delete.setAttribute("class", "note-btn");
+        btn_delete.setAttribute("id", "btn-delete");
+        text = document.createTextNode("Delete");
+        btn_delete.appendChild(text);
+        note.appendChild(btn_delete);
+
         document.body.appendChild(note);
     }
     else {
