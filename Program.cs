4050a5773de2df3845047d88e80ea3cd11abd5a0
AnastasiaSerenko <orangesun94@gmail.com>
2024-01-09 23:16:50 +0700

    1) disable caching of all static files; 2) added middleware 'MapDelete' and function 'deleteNote(id)'; 3) fixed style of website

--- Program.cs
+++ Program.cs
@@ -1,18 +1,41 @@
 using Notes;
+using System;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 List<Note> notes = new List<Note>();
 
 app.UseDefaultFiles();
-app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions()
+{
+    OnPrepareResponse = (context) =>
+    {
+        // Disable caching of all static files.
+        context.Context.Response.Headers["Cache-Control"] = "no-cache, no-store";
+        context.Context.Response.Headers["Pragma"] = "no-cache";
+        context.Context.Response.Headers["Expires"] = "-1";
+    }
+});
+
 
 app.MapPost("/addNote", (Note note) =>
 {
-
     note.Id = Guid.NewGuid().ToString();
+    note.Text = "New note";
     notes.Add(note);
     return note;
+});
+
+app.MapDelete("/notes/{id}", (string id) =>
+{
+    Note? note = notes.FirstOrDefault(u => u.Id == id);
+
+    if (note == null) return Results.NotFound(new { message = "Заметка не найдена" });
+
+    notes.Remove(note);
+    return Results.Json(note);
 });
 
 app.Run();
--- wwwroot/site.js
+++ wwwroot/site.js
@@ -6,35 +6,54 @@ document.getElementById("addNote").addEventListener("click", async () => {
         body: JSON.stringify({
         })
     });
+
     if (response.ok === true) {
         const note = document.createElement("div");
         note.setAttribute("class", "note");
+        const note_object = await response.json();
+        note.setAttribute("id", note_object.id);
 
         const note_text = document.createElement("p");
         note_text.setAttribute("class", "note-text");
         note_text.setAttribute("contenteditable", "true");
-        let text = document.createTextNode("Hello World");
+        let text = document.createTextNode(note_object.text);
         note_text.appendChild(text);
         note.appendChild(note_text);
 
         const btn_edit = document.createElement("button");
         btn_edit.setAttribute("class", "note-btn");
-        btn_edit.setAttribute("id", "btn-edit");
         text = document.createTextNode("Edit");
         btn_edit.appendChild(text);
         note.appendChild(btn_edit);
 
         const btn_delete = document.createElement("button");
         btn_delete.setAttribute("class", "note-btn");
-        btn_delete.setAttribute("id", "btn-delete");
         text = document.createTextNode("Delete");
         btn_delete.appendChild(text);
+        btn_delete.addEventListener("click", async () => await deleteNote(note_object.id));
         note.appendChild(btn_delete);
 
-        document.body.appendChild(note);
+        const desk = document.getElementById("desk");
+        desk.appendChild(note);
     }
     else {
         const error = await response.json();
         console.log(error.message);
     }
 });
+
+async function deleteNote(id) {
+    const response = await fetch(`/notes/${id}`, {
+        method: "DELETE",
+        headers: { "Accept": "application/json" }
+    });
+
+    if (response.ok === true) {
+        var note = document.getElementById(id);
+        note.remove();
+    }
+    else {
+        const error = await response.json();
+        console.log(error.message);
+    }
+};
